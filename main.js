5d9847efa650009b7e83acf5342f02e8eaeaa162
Eugene Fedotov <eugenefedoto@gmail.om>
2018-07-07 15:51:46 -0500

    Complete Task 2

---
+++ main.js
@@ -0,0 +1,64 @@
+Vue.component('tabs', {
+    template: `
+        <div>
+            <div class="tabs">
+                <ul class="tab-links">
+                    <li v-for="tab in tabs" :class="{ 'is-active': tab.isActive }">
+                        <a :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="tabs-details">
+                <slot></slot>
+            </div>
+        </div>
+    `,
+
+    data() {
+        return { tabs: [] };
+    },
+
+    created() {
+        this.tabs = this.$children;
+    },
+
+    methods: {
+        selectTab(selectedTab) {
+            this.tabs.forEach(tab => {
+                tab.isActive = (tab.href == selectedTab.href);
+            });
+        }
+    }
+});
+
+
+Vue.component('tab', {
+    template: `
+        <div class="content" v-show="isActive"><slot></slot></div>
+    `,
+
+    props: {
+        name: { required: true },
+        focus: { default: false }
+    },
+
+    data() {
+        return {
+            isActive: false
+        };
+    },
+
+    computed: {
+        href() {
+            return '#' + this.name.toLowerCase().replace(/ /g, '-');
+        }
+    },
+
+    mounted() {
+        this.isActive = this.focus;
+    },
+});
+
+new Vue({
+    el: '#root'
+});
